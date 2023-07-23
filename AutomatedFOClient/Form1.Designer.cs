6ec3dfdf5ec0f3e509196b0853f5e96f35a670b6
Kamesh Shankaran <skameshh@gmail.com>
2023-07-23 21:02:08 +0800

    init

---
+++ AutomatedFOClient/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace AutomatedFOClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtConfNumber = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chkPartReceived = new System.Windows.Forms.CheckBox();
+            this.chkPartProcStarted = new System.Windows.Forms.CheckBox();
+            this.chkPartProcEnd = new System.Windows.Forms.CheckBox();
+            this.chkPartQCDone = new System.Windows.Forms.CheckBox();
+            this.chkPartSentDelv = new System.Windows.Forms.CheckBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnSend.Location = new System.Drawing.Point(138, 362);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(173, 54);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Submit";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtConfNumber
+            // 
+            this.txtConfNumber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtConfNumber.Location = new System.Drawing.Point(154, 58);
+            this.txtConfNumber.Name = "txtConfNumber";
+            this.txtConfNumber.Size = new System.Drawing.Size(211, 34);
+            this.txtConfNumber.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(154, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 28);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Confirmation Number : ";
+            // 
+            // chkPartReceived
+            // 
+            this.chkPartReceived.AutoSize = true;
+            this.chkPartReceived.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkPartReceived.Location = new System.Drawing.Point(154, 110);
+            this.chkPartReceived.Name = "chkPartReceived";
+            this.chkPartReceived.Size = new System.Drawing.Size(165, 32);
+            this.chkPartReceived.TabIndex = 4;
+            this.chkPartReceived.Text = "Part Received";
+            this.chkPartReceived.UseVisualStyleBackColor = true;
+            // 
+            // chkPartProcStarted
+            // 
+            this.chkPartProcStarted.AutoSize = true;
+            this.chkPartProcStarted.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkPartProcStarted.Location = new System.Drawing.Point(154, 156);
+            this.chkPartProcStarted.Name = "chkPartProcStarted";
+            this.chkPartProcStarted.Size = new System.Drawing.Size(181, 32);
+            this.chkPartProcStarted.TabIndex = 5;
+            this.chkPartProcStarted.Text = "Process Started";
+            this.chkPartProcStarted.UseVisualStyleBackColor = true;
+            this.chkPartProcStarted.CheckedChanged += new System.EventHandler(this.chkPartProcStarted_CheckedChanged);
+            // 
+            // chkPartProcEnd
+            // 
+            this.chkPartProcEnd.AutoSize = true;
+            this.chkPartProcEnd.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkPartProcEnd.Location = new System.Drawing.Point(154, 201);
+            this.chkPartProcEnd.Name = "chkPartProcEnd";
+            this.chkPartProcEnd.Size = new System.Drawing.Size(153, 32);
+            this.chkPartProcEnd.TabIndex = 6;
+            this.chkPartProcEnd.Text = "Process END";
+            this.chkPartProcEnd.UseVisualStyleBackColor = true;
+            this.chkPartProcEnd.CheckedChanged += new System.EventHandler(this.chkPartProcEnd_CheckedChanged);
+            // 
+            // chkPartQCDone
+            // 
+            this.chkPartQCDone.AutoSize = true;
+            this.chkPartQCDone.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkPartQCDone.Location = new System.Drawing.Point(154, 245);
+            this.chkPartQCDone.Name = "chkPartQCDone";
+            this.chkPartQCDone.Size = new System.Drawing.Size(117, 32);
+            this.chkPartQCDone.TabIndex = 7;
+            this.chkPartQCDone.Text = "QC Done";
+            this.chkPartQCDone.UseVisualStyleBackColor = true;
+            this.chkPartQCDone.CheckedChanged += new System.EventHandler(this.chkPartQCDone_CheckedChanged);
+            // 
+            // chkPartSentDelv
+            // 
+            this.chkPartSentDelv.AutoSize = true;
+            this.chkPartSentDelv.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkPartSentDelv.Location = new System.Drawing.Point(154, 293);
+            this.chkPartSentDelv.Name = "chkPartSentDelv";
+            this.chkPartSentDelv.Size = new System.Drawing.Size(196, 32);
+            this.chkPartSentDelv.TabIndex = 8;
+            this.chkPartSentDelv.Text = "Sent for Delivery";
+            this.chkPartSentDelv.UseVisualStyleBackColor = true;
+            this.chkPartSentDelv.CheckedChanged += new System.EventHandler(this.chkPartSentDelv_CheckedChanged);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMsg.Location = new System.Drawing.Point(147, 449);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(39, 20);
+            this.lblMsg.TabIndex = 9;
+            this.lblMsg.Text = "Msg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 508);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.chkPartSentDelv);
+            this.Controls.Add(this.chkPartQCDone);
+            this.Controls.Add(this.chkPartProcEnd);
+            this.Controls.Add(this.chkPartProcStarted);
+            this.Controls.Add(this.chkPartReceived);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtConfNumber);
+            this.Controls.Add(this.btnSend);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Halliburton FO Status";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtConfNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox chkPartReceived;
+        private System.Windows.Forms.CheckBox chkPartProcStarted;
+        private System.Windows.Forms.CheckBox chkPartProcEnd;
+        private System.Windows.Forms.CheckBox chkPartQCDone;
+        private System.Windows.Forms.CheckBox chkPartSentDelv;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ AutomatedFOClient/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutomatedFOClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            doSend();
+           
+        }
+
+        private void doSend()
+        {
+            if (txtConfNumber.Text.Length < 1)
+            {
+                MessageBox.Show("Confirmation number cant be null ");
+                return;
+            }
+
+            if (!chkPartReceived.Checked)
+            {
+                MessageBox.Show("Part must be received, Please try again ");
+                chkPartReceived.Checked = true;
+                return;
+            }
+
+             doSendEmail(makeMessage());
+        }
+
+        /**
+         *  message  = "confnum:"+conf_number
+                    +"\n step1:"+step1_part_received
+                    +"\n step2:"+step2_proc_started
+                    +"\n step3:"+step3_proc_finished
+                    +"\n step4:"+step4_qc_finished
+                    +"\n step5:"+step5_sent_delivery
+                    +"\n";
+         * 
+         * 
+         */
+        private void doSendEmail(string htmlString)
+        {
+            Console.WriteLine("Email sending ...");
+            try
+            {
+                lblMsg.Text = "";
+               MailMessage message = new MailMessage();
+                SmtpClient smtp = new SmtpClient();
+                message.From = new MailAddress("halauto.foparts@gmail.com");
+                message.To.Add(new MailAddress("kamesh.shankaran@halliburton.com"));
+                message.CC.Add(new MailAddress("skameshh@gmail.com"));
+                message.Subject = "[FOParts]-Test";
+                message.IsBodyHtml = false; //to make message body as html
+                message.Body = htmlString;
+                smtp.Port = 587;
+                smtp.Host = "smtp.gmail.com"; //for gmail host
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential("halauto.foparts@gmail.com", "hvbzudpwcdwzjwzu");
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Send(message);
+                Console.WriteLine("Email send success ...");
+                lblMsg.ForeColor = Color.DarkGreen;
+                lblMsg.Text = "Submitted successfully";
+            }
+            catch(Exception ee)
+            {
+                Console.WriteLine("Email send failed ..."+ee.Message);
+                // MessageBox.Show("Error in submitting  " + ee.Message);
+                lblMsg.ForeColor = Color.Red;
+                lblMsg.Text = "Submit failed, Please check internet connection.";
+                return;
+            }
+           
+        }
+
+        private string makeMessage()
+        {
+            //"confnum:13579135 \n step1:ON \n step2:ON \n step3:OFF \n step4:OFF \n step5:OFF"
+            StringBuilder str = new StringBuilder();
+            str.Append("confnum:" +txtConfNumber.Text);
+            str.Append("step1:"+ (chkPartReceived.Checked?"ON":"OFF"));
+            str.Append("step2:" + (chkPartProcStarted.Checked ? "ON" : "OFF"));
+            str.Append("step3:" + (chkPartProcEnd.Checked ? "ON" : "OFF"));
+            str.Append("step4:" + (chkPartQCDone.Checked ? "ON" : "OFF"));
+            str.Append("step5:" + (chkPartSentDelv.Checked ? "ON" : "OFF"));
+
+            return str.ToString();
+        }
+
+        private void doStep2(bool step2)
+        {
+            if (step2)
+            {
+                chkPartReceived.Checked = true;
+            }
+        }
+
+        private void doStep3(bool step3)
+        {
+            if (step3)
+            {
+                chkPartReceived.Checked = true;
+                chkPartProcStarted.Checked = true;
+            }
+        }
+
+        private void doStep4(bool step4)
+        {
+            if (step4)
+            {
+                chkPartReceived.Checked = true;
+                chkPartProcStarted.Checked = true;
+                chkPartProcEnd.Checked = true;
+            }
+        }
+
+        private void doStep5(bool step5)
+        {
+            if (step5)
+            {
+                chkPartReceived.Checked = true;
+                chkPartProcStarted.Checked = true;
+                chkPartQCDone.Checked = true;
+            }
+        }
+
+
+        private void chkPartProcStarted_CheckedChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("process = "+ chkPartProcStarted.Checked);
+            //lblMsg.Text = "process = " + chkPartProcStarted.Checked;
+            doStep2(chkPartProcStarted.Checked);
+        }
+
+              
+
+        private void chkPartProcEnd_CheckedChanged(object sender, EventArgs e)
+        {
+            doStep3(chkPartProcEnd.Checked);
+        }
+
+        private void chkPartQCDone_CheckedChanged(object sender, EventArgs e)
+        {
+            doStep4(chkPartQCDone.Checked);
+        }
+
+        private void chkPartSentDelv_CheckedChanged(object sender, EventArgs e)
+        {
+            doStep5(chkPartSentDelv.Checked);
+        }
+    }
+}
